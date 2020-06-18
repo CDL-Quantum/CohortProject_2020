ac94a26fb9fdc215693a6f9b5baae04b4fbe2a65
Isaac De Vlugt <ijsdevlugt@uwaterloo.ca>
2020-06-17 23:00:59 -0400

    Now task 2 is for 100 atoms, fixed Ryd. energy estimator

--- Project_1_RBM_and_Tomography/Rydberg_energy_calculator.py
+++ Project_1_RBM_and_Tomography/Rydberg_energy_calculator.py
@@ -1,73 +1,34 @@
 import numpy as np
 import torch
 
-def Vij(R, N):
+def Vij(R,i,j):
+    return 1./(R*abs(i-j))**6.
 
-    vij = torch.zeros(N, N)
-    
-    for i in range(N-1):
-        for j in range(i+1,N):
-        
-            vij[i][j] = 1./(R*(j-i))**6.
-
-    return vij 
-
-    #vij = torch.zeros(N-1)
-    #for k in range(0, N-1):
-    #    vij[k] = 1./(R*(k+1))**6.
-
-    #return vij
-     
-def target_psi(samples):
-    # only for N = 2
-    coeffs = torch.zeros(samples.shape[0])
-    for i in range(samples.shape[0]):
-        if torch.equal(samples[i,:], torch.tensor([0,0])):
-            coeffs[i] = 0.973349
-        if torch.equal(samples[i,:], torch.tensor([1,0])):
-            coeffs[i] = 0.159871
-        if torch.equal(samples[i,:], torch.tensor([0,1])):
-            coeffs[i] = 0.159871
-        if torch.equal(samples[i,:], torch.tensor([1,1])):
-            coeffs[i] = 0.0383912
-    return coeffs
- 
 def energy(samples, RBM_wvfn):
 
     # hardset Rydberg Hamiltonian parameters
-    R = 1
-    h = 1
-    Omega = 1
+    R = 1.
+    h = 1.
+    Omega = 1.
     
     samples = samples.type(torch.int64)
-
     N = samples.shape[1]
-    vij = Vij(R, N)
 
     # to +/- 1
-    samples = samples*2 - 1
+    samples = -(samples*2 - 1)
 
-    diagonal1 = 0
-    diagonal2 = 0
+    diagonal = 0
     # pair-wise term
     for i in range(N-1):
+        diagonal -= Omega*samples[:,i]
         for j in range(i+1,N):
+            diagonal -= Vij(R,i,j)*(samples[:,i]*samples[:,j] + samples[:,i] + samples[:,j])
             
-            diagonal1 = diagonal1 - vij[i][j] * (
-                samples[:,i] * samples[:,j] + samples[:,i] + samples[:,j]
-            )
-            #diagonal1 = diagonal1 - vij[j-i-1] * (
-            #    samples[:,i] * samples[:,j] + samples[:,i] + samples[:,j]
-            #)
-            
-        # longitudinal field term
-        diagonal2 = diagonal2 - Omega * samples[:,i]
-
     # don't get the Nth term in the above loop
-    diagonal2 = diagonal2 - Omega * samples[:,N-1]
+    diagonal -= Omega*samples[:,N-1]
 
     # back to 0,1
-    samples = (samples + 1) / 2
+    samples = (-samples + 1) / 2
 
     # transverse-field term
     off_diagonal = 0
@@ -84,4 +45,4 @@ def energy(samples, RBM_wvfn):
         # undo the flip
         samples[:,n] = samples[:,n] ^ 1
 
-    return (diagonal1 + diagonal2 + off_diagonal).sum() / (N*samples.shape[0])
+    return (diagonal + off_diagonal).sum() / (N*samples.shape[0])
